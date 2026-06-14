13682a115a89219e7bb75ee9587da700c1aaa760
Truong Van Thanh <truong.van.thanh@sun-asterisk.com>
2026-06-14 21:36:28 +0900

    init

---
+++ src/test/java/com/banking/banking_lab/BankingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banking_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
